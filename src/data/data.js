2d66f132b2f0668ef18ffc77156ee446c7a30e4c
raushan <kraushan022@gmail.com>
2025-03-27 17:54:12 +0530

    added dummy data

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+export const foodCategories = {
+   Starters: [
+      { id: 1, name: "Spring Rolls" },
+      { id: 2, name: "Garlic Bread" },
+      { id: 3, name: "Bruschetta" }
+   ],
+   MainCourse: [
+      { id: 4, name: "Butter Chicken" },
+      { id: 5, name: "Paneer Tikka" },
+      { id: 6, name: "Mutton Biryani" }
+   ],
+   Sweets: [
+      { id: 7, name: "Gulab Jamun" },
+      { id: 8, name: "Rasgulla" },
+      { id: 9, name: "Jalebi" }
+   ],
+   HiTea: [
+      { id: 10, name: "Masala Tea" },
+      { id: 11, name: "Coffee" },
+      { id: 12, name: "Cookies" }
+   ]
+};
